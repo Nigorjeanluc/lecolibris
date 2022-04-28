03c3be390ca7ea3a4312819eec97c9d56ea47bb5
Igor J.Luc Ndiramiye <nigorjeanluc@gmail.com>
2022-04-28 04:38:02 +0200

    More on main page

--- src/Components/Carousel/index.js
+++ src/Components/Carousel/index.js
@@ -17,7 +17,7 @@ function Slider() {
             alt="First slide"
             />
             <Carousel.Caption>
-            <h5>First slide label</h5>
+            {/* <h5>First slide label</h5> */}
             {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
             </Carousel.Caption>
         </Carousel.Item>
@@ -28,7 +28,7 @@ function Slider() {
             alt="Second slide"
             />
             <Carousel.Caption>
-            <h5>Second slide label</h5>
+            {/* <h5>Second slide label</h5> */}
             {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
             </Carousel.Caption>
         </Carousel.Item>
@@ -39,7 +39,7 @@ function Slider() {
             alt="Third slide"
             />
             <Carousel.Caption>
-            <h5>Third slide label</h5>
+            {/* <h5>Third slide label</h5> */}
             {/* <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
             </Carousel.Caption>
         </Carousel.Item>
--- src/Components/MainPage/index.js
+++ src/Components/MainPage/index.js
@@ -3,6 +3,10 @@ import {motion} from "framer-motion";
 import { Container, Row, Col, Nav } from 'react-bootstrap';
 
 import logo from '../../assets/logo.png';
+import image1 from '../../assets/image2.jpg';
+import image2 from '../../assets/image3.jpg';
+import image3 from '../../assets/image4.jpg';
+
 import '../../Styles/MainPage/MainPage.css';
 
 import Carousel from '../../Components/Carousel';
@@ -38,6 +42,35 @@ function MainPage() {
             <div className="sliderSection">
               <Carousel />
             </div>
+          </Col>
+          <Col md={12} lg={12}>
+            <div className="banner">
+              <h1>Rapidité, joie, amour et excellence pour l'instruction de l'avenir</h1>
+            </div>
+          </Col>
+          <Col className="cards" md={12} lg={12}>
+            <Row>
+              <Col>
+                <div className="card1">
+                  <img alt="creche" src={image1} />
+                  <h2>La Crèche</h2>
+                </div>
+              </Col>
+              <Col>
+                <div className="card2">
+                  <img alt="maternelle" src={image2} />
+                  <h2>L' école maternelle</h2>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} sm={6} md={6} lg={6}>
+                <div className="card3">
+                  <img alt="primaire" src={image3} />
+                  <h2>L' école primaire</h2>
+                </div>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Container>
